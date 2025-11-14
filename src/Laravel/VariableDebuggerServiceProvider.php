e5de96dbc86a500ce600a39482a743b840aa7be9
lightsouler <lightsouler@hotmail.com>
2025-11-14 21:44:02 +0700

    update laravel extensions

---
+++ src/Laravel/VariableDebuggerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace LightLa\VariableDebugger\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Configuration\Exceptions;
+use LightLa\VariableDebugger\Exceptions\VariableDebugGracefulExitException;
+
+class VariableDebuggerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->app->afterResolving('exceptions', function (Exceptions $exceptions) {
+            $exceptions->render(function (VariableDebugGracefulExitException $e) {
+                return response('', 200);
+            });
+        });
+    }
+}
